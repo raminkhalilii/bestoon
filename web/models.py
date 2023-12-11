121a6cbf0b0efefd3c6429167c0c7f1e6c2604bb
raaminkhalili2001@gmail.com <raaminkhalili@aut.ac.ir>
2023-12-11 16:23:42 +0100

    second

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Expenses(models.Model):
+    Name = models.CharField(max_length=200)
+    amount = models.IntegerField
+    user = models.ForeignKey(User)
+    date = models.DateTimeField(auto_now=True)
